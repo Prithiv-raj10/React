eb1f7effae80ee8603b0057b4396f3c4b2561265
Prithiv <Prithiv@DESKTOP-4QO94B5>
2023-06-03 22:51:36 +0530

    initial

--- src/Apis/index.ts
+++
@@ -1,6 +0,0 @@
-import carItemApi from "./carItemApi";
-import bookingApi from "./bookingApi";
-import authApi from "./authApi";
-import paymentApi from "./paymentApi";
-import orderApi from "./orderApi";
-export {carItemApi, bookingApi,authApi,paymentApi,orderApi};
