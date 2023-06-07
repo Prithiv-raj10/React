d6e6dd278b092da649861fd61c7f7253aaf8a184
Prithiv raj s <64250687+Prithiv-raj10@users.noreply.github.com>
2023-06-07 11:50:53 +0530

    changes in connection.

---
+++ src/Apis/index.ts
@@ -0,0 +1,6 @@
+import carItemApi from "./carItemApi";
+import bookingApi from "./bookingApi";
+import authApi from "./authApi";
+import paymentApi from "./paymentApi";
+import orderApi from "./orderApi";
+export {carItemApi, bookingApi,authApi,paymentApi,orderApi};
